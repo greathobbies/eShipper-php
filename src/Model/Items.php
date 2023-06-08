ab82df2e109f32cdc44024877b2a56e4049d8e1c
David McKenna <david.mckenna@greathobbies.com>
2023-06-07 22:37:28 -0300

    Adding Items model

---
+++ src/Model/Items.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Davidflypei\Eshipper\Model;
+
+use Davidflypei\Eshipper\Common\EshipperModel;
+
+class Items extends EshipperModel
+{
+
+  /**
+   * @param $items
+   * @return $this
+   */
+  public function setItems($items)
+  {
+    $this->items = $items;
+    return $this;
+  }
+
+  /**
+   * @return \Davidflypei\Eshipper\Model\Item[]
+   */
+  public function getItems()
+  {
+    return $this->items;
+  }
+
+  /**
+   * @param $item
+   * @return $this
+   */
+  public function addPackage($item)
+  {
+    if (!$this->getItems()) {
+      return $this->setItems(array($item));
+    } else {
+      return $this->setItems(
+        array_merge($this->getItems(), array($item))
+      );
+    }
+  }
+
+  /**
+   * @param $item
+   * @return $this
+   */
+  public function removeItem($item)
+  {
+    return $this->setItems(
+      array_diff($this->getItems(), array($item))
+    );
+  }
+
+  /**
+   * @param $currency
+   * @return $this
+   */
+  public function setCurrency($currency)
+  {
+    $this->currency = $currency;
+    return $this;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getCurrency()
+  {
+    return $this->currency;
+  }
+
+}
